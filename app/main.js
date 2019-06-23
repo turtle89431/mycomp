3054a5d05f49fd4435d6969504da81d34e9c83bb
unknown <turtle89431@gmail.com>
2019-06-22 22:53:24 -0700

    added parcel

--- app/main.js
+++ app/main.js
@@ -1,31 +1,41 @@
-const { app, BrowserWindow, MenuItem, Menu, ipcMain, systemPreferences, dialog } = require('deskgap');
+const {
+  app,
+  BrowserWindow,
+  MenuItem,
+  Menu,
+  ipcMain,
+  systemPreferences,
+  dialog
+} = require("deskgap");
 
-ipcMain.on('hello-to-node', (e, message) => {
-    e.sender.send('hello-from-node', "Hello, " + message);
+ipcMain.on("hello-to-node", (e, message) => {
+  e.sender.send("hello-from-node", "Hello, " + message);
 });
 
 let mainWindow;
 
-app.once('ready', () => {
+app.once("ready", () => {
+  mainWindow = new BrowserWindow({
+    show: false,
+    width: 800,
+    height: 600
+  }).once("ready-to-show", () => {
+    mainWindow.show();
+  });
 
-    mainWindow = new BrowserWindow({
-        show: false,
-        width: 800, height: 600,
-    }).once('ready-to-show', () => {
-        mainWindow.show();
+  if (process.platform !== "win32") {
+    mainWindow.webView.setDevToolsEnabled(true);
+  }
+
+  for (const eventName of ["blur", "focus"]) {
+    mainWindow.on(eventName, () => {
+      mainWindow.webView.send("window-" + eventName);
     });
+  }
 
-    if (process.platform !== 'win32') {
-        mainWindow.webView.setDevToolsEnabled(true);
-    }
+  mainWindow.loadFile("app.html");
 
-    for (const eventName of ['blur', 'focus']) {
-        mainWindow.on(eventName, () => { mainWindow.webView.send('window-' + eventName) })
-    }
-
-    mainWindow.loadFile("app.html");
-
-    mainWindow.on('closed', () => {
-        mainWindow = null;
-    });
+  mainWindow.on("closed", () => {
+    mainWindow = null;
+  });
 });
